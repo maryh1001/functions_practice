269a66b63f950a81da0fa5a7002f5b05a0bd01f1
maryh1001 <focusedfitness.mh@gmail.com>
2022-08-20 15:44:27 -0400

    attempt 1

--- functions_practice.py
+++ functions_practice.py
@@ -1,7 +1,15 @@
 # print a greeting to user
+from socket import J1939_EE_INFO_NONE
+
+
 def hello():
     print ("Hi User!!")
 # accepts three arguments and returns a list with the arguments inside as list elements
-def pack():
-# accepta a list of any length and print a series of strings that say "First I eat__" .. etc to "My lunch box is empty."
+def pack(item1, item2, item3):
+    item1 = "PB&J"
+    item2 = "orange"
+    item3 = "juice box" 
+
+    print(item1,item2, item3)
+# accept a list of any length and print a series of strings that say "First I eat__" .. etc to "My lunch box is empty."
 def eat_lunch():
