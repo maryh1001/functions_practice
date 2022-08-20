39616cf73cc9394fa9ff4bbd31567e783ef91ca4
maryh1001 <97072199+maryh1001@users.noreply.github.com>
2022-08-20 13:37:28 -0400

    Add files via upload

---
+++ functions_practice.py
@@ -0,0 +1,6 @@
+# print a greeting to user
+hello()
+# accepts three arguments and returns a list with the arguments inside as list elements
+pack()
+# accepta a list of any length and print a series of strings that say "First I eat__" .. etc to "My lunch box is empty."
+eat_lunch()
