de9c97c141df3627f832099c99cb3c6b97fa192d
maryh1001 <focusedfitness.mh@gmail.com>
2022-08-20 15:18:24 -0400

    test

--- functions_practice.py
+++ functions_practice.py
@@ -1,6 +1,7 @@
 # print a greeting to user
-hello()
+def hello():
+    print ("Hi User!!")
 # accepts three arguments and returns a list with the arguments inside as list elements
-pack()
+def pack():
 # accepta a list of any length and print a series of strings that say "First I eat__" .. etc to "My lunch box is empty."
-eat_lunch()
+def eat_lunch():
